10df909f7d11c17655da1e04c35daccdbc29e3a6
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-12-17 23:31:58 +0530

    end

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,29 @@ const express = require('express');
 
 const speakersRoute = require('./speakers');
 const feedbackRoute = require('./feedback');
-const aboutRoute = require('./about');
-const miltonRoute = require('./practice');
 
 const router = express.Router();
 
 module.exports = params => {
-  router.get('/', (request, response) => {
-    // if (!request.session.visitcount) {
-    //   request.session.visitcount = 0;
-    // }
-    // request.session.visitcount += 1;
-    // console.log(`Number of visits: ${request.session.visitcount}`);
+  const { speakersService } = params;
 
-    response.render('layout', { pageTitle: 'Welcome', template: 'index' }); //default to index.ejs in layout
+  router.get('/', async (request, response, next) => {
+    try {
+      const artwork = await speakersService.getAllArtwork();
+      const topSpeakers = await speakersService.getList();
+      return response.render('layout', {
+        pageTitle: 'Welcome',
+        template: 'index',
+        topSpeakers,
+        artwork,
+      });
+    } catch (err) {
+      return next(err);
+    }
   });
 
   router.use('/speakers', speakersRoute(params));
   router.use('/feedback', feedbackRoute(params));
-  router.use('/about', aboutRoute(params));
-  router.use('/milton', miltonRoute(10));
 
   return router;
 };
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+
+const bodyParser = require('body-parser');
+
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
 
@@ -11,6 +15,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.locals.siteName = 'ROUX Academy';
+
 const port = 3000;
 
 app.set('trust proxy', 1);
@@ -18,19 +24,28 @@ app.set('trust proxy', 1);
 app.use(
   cookieSession({
     name: 'session',
-    keys: ['Ghdurdfdf', 'efefdvfd'],
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
   })
 );
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-app.use(express.static(path.join(__dirname, './static')));
-
 app.locals.siteName = 'ROUX Meetups';
 
-app.use((request, response, next) => {
-  response.locals.someVariable = 'hello';
-  return next();
+app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakersService.getNames();
+    response.locals.speakerNames = names;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 app.use(
@@ -40,6 +55,19 @@ app.use(
     speakersService,
   })
 );
+
+app.use((request, response, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, request, response, next) => {
+  response.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  response.locals.status = status;
+  response.status(status);
+  response.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
